98a8a3ded58baf7665a7469f285b4a59f588a080
wolfo1 <70093754+wolfo1@users.noreply.github.com>
2021-12-30 19:23:35 +0200

    Update PepseGameManager.java

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -12,8 +12,14 @@ import danogl.util.Vector2;
 import pepse.world.Block;
 import pepse.world.Sky;
 import pepse.world.Terrain;
+import pepse.world.daynight.Moon;
 import pepse.world.daynight.Night;
 import pepse.world.daynight.Sun;
+import pepse.world.daynight.SunHalo;
+import pepse.world.trees.Tree;
+
+import java.awt.*;
+import java.util.Random;
 
 public class PepseGameManager extends GameManager {
 
@@ -26,7 +32,7 @@ public class PepseGameManager extends GameManager {
         this.windowController = windowController;
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
         windowDimensions = this.windowController.getWindowDimensions(); // gets window dimensions
-       //create sky
+        //create sky
         Sky.create( gameObjects(), windowDimensions , Layer.BACKGROUND);
         // create terrain
         Terrain terrain = new Terrain(this.gameObjects(), Layer.STATIC_OBJECTS, windowDimensions);
@@ -41,6 +47,12 @@ public class PepseGameManager extends GameManager {
         GameObject night = Night.create(gameObjects(), Layer.FOREGROUND, windowDimensions, 30);
         // create sun
         GameObject sun = Sun.create(gameObjects(), Layer.BACKGROUND, windowDimensions, 30);
+        // create halo
+        GameObject sunHalo = SunHalo.create(gameObjects(), Layer.BACKGROUND + 1, sun, new Color(255, 255, 0, 20));
+        // create moon
+        GameObject moon = Moon.create(gameObjects(), Layer.BACKGROUND, windowDimensions, 30, imageReader);
+        // create moon halo
+        GameObject moonHalo = SunHalo.create(gameObjects(), Layer.BACKGROUND + 1, moon, new Color(255, 255, 255, 80));
     } // overrides initializeGame
 
 
@@ -49,7 +61,7 @@ public class PepseGameManager extends GameManager {
      * @param args This argument should not be used.
      */
     public static void main(String[] args){
-    new PepseGameManager().run();
+        new PepseGameManager().run();
     } // end of main
 
 } // end of PepseGameManager
