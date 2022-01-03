8a617dfa21bea6d42ad8f9b77d60b73442a0398a
galbum <63543882+galbum@users.noreply.github.com>
2022-01-03 10:54:56 +0200

    Add files via upload

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -68,7 +68,13 @@ public class PepseGameManager extends GameManager {
     private static final int extendBy = 20 * Block.SIZE;
     private Terrain terrain;
 
-
+    /**
+     * initializeGame in class danogl.GameManager
+     * @param imageReader - Contains a single method: readImage, which reads an image from disk. See its documentation for help.
+     * @param soundReader - Contains a single method: readSound, which reads a wav file from disk. See its documentation for he
+     * @param inputListener - Contains a single method: isKeyPressed, which returns whether a given key is currently pressed by the user or not. See its documentation.
+     * @param windowController - Contains an array of helpful, self explanatory methods concerning the window.
+     */
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         this.windowController = windowController;
